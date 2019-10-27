e83e5c62658db22955196c30a881242d50548f53
josecalderon85 <jose.calderon584061@gmail.com>
2019-10-27 02:27:29 -0600

    pajarito changes

--- 5. Construir un juego JavaScript/pajarito.js
+++ 5. Construir un juego JavaScript/pajarito.js
@@ -4,12 +4,34 @@ const ctx = cvs.getContext("2d");
 //GAME VAR AND CONSTS
 
 let frames= 0;
-
+const DEGREE = Math.PI/180;
 
 //LOAD SPRITE IMAGE
 
 const sprite = new Image();
 sprite.src = "img/sprite.png";
+
+// GAME STATE
+const state = {
+    current : 0,
+    getReady : 0,
+    game : 1 ,
+    over : 2
+}
+//CONTROL THE GAME
+cvs.addEventListener("click",function(evt){
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            bird.flap();
+            break;
+        case state.over:
+            state.curent = state.getReady;
+            break;
+    }
+});
 
 //BACKGROUND
 const bg = {
@@ -35,17 +57,57 @@ const fg = {
     h : 112,
     x : 0,
     y : cvs.height - 112,
+    dx : 2,
     
     draw : function(){
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
        
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x + this.w ,this.y,this.w,this.h);
     
+    },
+    update : function(){
+        if(state.current == state.game){
+        this.x = (this.x - this.dx)%(this.w /2);
     }
+}
+}
+//PIPES
+
+const pipes{
+    position : [],
+bottom : {
+    sX : 502,
+    sY : 0
+},
+top : {
+    sX : 553,
+    sY : 0
+},
+w : 53,
+h : 400,
+gap : 85,
+dx : 2,
+maxYPos : -150,
+
+update :function(){
+};
+draw :function(){
+
+}
+}
+if(frames%100 == 0){
+    this.position.push(
+        {
+            x : cvs.width,
+            y : this.maxYPos * ( Math.random() +1 )
+        }
+    );
+}
 }
 // Bird
 
 const bird = {
+   
     animation : [
         { sX : 276, sY : 112},//this.animation[0],
         { sX : 276, sY : 139},//this.animation[1],
@@ -57,11 +119,50 @@ const bird = {
     w: 34,
     h: 26,
     frame : 0,
+    gravity : 0.25,
+    jump : 4.6,
+    speed : 0,
+    rotation: 0,
     draw : function(){
         let bird = this.animation[this.frame];
-        ctx.drawImage(sprite, bird.sX,bird.sY,this.w,this.h,this.x - this.w/2,this.y - this.h/2,this.w,this.h);
- 
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h);
+        ctx.restore();
+    },
+    flap : function(){
+        this.speed = - this.jump;
+    },
+    update: function(){
+        // IF THE GAME STATE IS GET READY STATE, THE PUTO PAJARO MUST FLAP SLOWLY
+        this.period = state.current == state.getReady ? 10 : 5;
+        //WE INCREMEMT THE FRAME BY 1 EACH PERIOD
+        this.frame += frames%this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN WE NEED TO GO BACK TO 0
+        this.frame = this.frame%this.animation.length;
+
+        if(state.current == state.getReady){
+            this.y = 150;
+            this.rotation = 0 * DEGREE;
+        }else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+            if(this.y + this.h/2 >= cvs.height - fg.h){
+                this.y = cvs.height - fg.h - this.h/2;
+                if(state.current == state.game){
+                    state.current = state.over;
+                }
+            }
+            if(this.speed >= this.jump){
+                this.rotation = 90 * DEGREE;
+                this.frame = 1;
+            }else{
+                this.rotation = -25 * DEGREE;
+            }
+        }
     }
+
 }
 //GET READY MESSAGE
 const getReady = {
@@ -73,9 +174,10 @@ const getReady = {
     h : 152,
 
     draw : function(){
+        if(state.current == state.getReady){
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
-       
-}
+        }  
+    }
 }
 //GAME OVER MESSAGE
 const gameOver = {
@@ -87,9 +189,10 @@ const gameOver = {
     y : 80,
 
     draw : function(){
+        if(state.current == state.over){
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
-       
-}
+        }  
+    }
 }
 
 //DRAW
@@ -107,7 +210,8 @@ function draw(){
 //UPDATE
 
 function update(){
-
+bird.update();
+fg.update();
     
 }
 //LOOP
