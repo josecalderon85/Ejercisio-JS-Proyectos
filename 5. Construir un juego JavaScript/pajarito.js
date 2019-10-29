333958341c28391b50240be806d7a87833757783
josecalderon85 <jose.calderon584061@gmail.com>
2019-10-29 15:59:29 -0600

    finished game

--- 5. Construir un juego JavaScript/pajarito.js
+++ 5. Construir un juego JavaScript/pajarito.js
@@ -18,17 +18,54 @@ const state = {
     game : 1 ,
     over : 2
 }
+
+// LOAD THE SOUNDS
+
+const SCORE_S = new Audio();
+SCORE_S.src = "audio/sfx_point.wav";
+
+const FLAP = new Audio();
+FLAP.src = "audio/sfx_flap.wav";
+
+const HIT = new Audio();
+HIT.src = "audio/sfx_hit.wav";
+
+const SWOOSHING = new Audio();
+SWOOSHING.src = "audio/sfx_swooshing.wav";
+
+const DIE = new Audio();
+DIE.src = "audio/sfx_die.wav";
+
+// Start Button
+const startBtn = {
+    x : 120,
+    y : 263,
+    w : 83,
+    h : 29
+}
 //CONTROL THE GAME
 cvs.addEventListener("click",function(evt){
     switch(state.current){
         case state.getReady:
             state.current = state.game;
+            SWOOSHING.play();
             break;
         case state.game:
             bird.flap();
+            FLAP.play();
             break;
         case state.over:
-            state.curent = state.getReady;
+            let rect = cvs.getBoundingClientRect();
+            let clickX = evt.clientX - rect.left;
+            let clickY = evt.clientY - rect.top;
+
+            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
+
+                    pipes.reset();
+                    bird.speedReset();
+                    score.reset();
+                    state.current = state.getReady;
+            }
             break;
     }
 });
@@ -71,39 +108,6 @@ const fg = {
     }
 }
 }
-//PIPES
-
-const pipes{
-    position : [],
-bottom : {
-    sX : 502,
-    sY : 0
-},
-top : {
-    sX : 553,
-    sY : 0
-},
-w : 53,
-h : 400,
-gap : 85,
-dx : 2,
-maxYPos : -150,
-
-update :function(){
-};
-draw :function(){
-
-}
-}
-if(frames%100 == 0){
-    this.position.push(
-        {
-            x : cvs.width,
-            y : this.maxYPos * ( Math.random() +1 )
-        }
-    );
-}
-}
 // Bird
 
 const bird = {
@@ -118,6 +122,7 @@ const bird = {
     y: 150,
     w: 34,
     h: 26,
+    radius: 8,
     frame : 0,
     gravity : 0.25,
     jump : 4.6,
@@ -152,6 +157,7 @@ const bird = {
                 this.y = cvs.height - fg.h - this.h/2;
                 if(state.current == state.game){
                     state.current = state.over;
+                    DIE.play();
                 }
             }
             if(this.speed >= this.jump){
@@ -161,6 +167,9 @@ const bird = {
                 this.rotation = -25 * DEGREE;
             }
         }
+    },
+    speedReset : function(){
+        this.speed = 0;
     }
 
 }
@@ -184,17 +193,114 @@ const gameOver = {
     sX : 175,
     sY : 228,
     w : 225,
-    h : 202,
+    h : 240,
     x : cvs.width/2 - 225/2,
-    y : 80,
+    y : 90,
 
     draw : function(){
-        if(state.current == state.over){
+        if(state.current === state.over){
         ctx.drawImage(sprite, this.sX,this.sY,this.w,this.h,this.x,this.y,this.w,this.h);
         }  
     }
 }
+//PIPES
 
+const pipes = {
+    position : [],
+    
+    top : {
+        sX : 553,
+        sY : 0
+    },
+    bottom:{
+        sX : 502,
+        sY : 0
+    },
+    
+    w : 53,
+    h : 400,
+    gap : 85,
+    maxYPos : -150,
+    dx : 2,
+    
+    draw :function(){
+        for(let i = 0; i < this.position.length; i++){
+            let p = this.position[i];
+
+            let topYPos = p.y;
+            let bottomYPos = p.y+this.gap+this.h;
+        
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+            
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
+        }
+    },
+update :function(){
+    for(let i=0;i<this.position.length;i++){
+        let p = this.position[i];
+        let bottomPipeY = p.y+ this.gap + this.h;
+       if(bird.x + bird.radius> p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius> p.y && bird.y - bird.radius<p.y+this.h
+       ){
+           state.current = state.over;
+       }
+       if(bird.x + bird.radius> p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeY&& bird.y + bird.radius < bottomPipeY + this.h
+        ){
+           if(state.current = state.over){
+            HIT.play();
+           }
+        }
+    }
+    if(state.current !== state.game) return;
+        if(frames%100 == 0){
+            this.position.push(
+        {
+            x : cvs.width,
+            y : this.maxYPos * ( Math.random() +1 )
+        });
+}
+    for(let i = 0;i< this.position.length; i++){
+    let p = this.position[i];
+    p.x -= this.dx;
+    
+    if(p.x + this.w <= 0){
+        this.position.shift();
+        score.value += 1;
+        SCORE_S.play();
+        score.best = Math.max(score.value, score.best);
+        localStorage.setItem("best", score.best);
+        }
+    }
+},
+reset : function(){
+    this.position = [];
+}
+}
+
+// SCORE
+
+const score ={
+    best :parseInt(localStorage.getItem("best"))|| 0,
+    value : 0,
+    draw : function(){
+        ctx.fillStyle ="#FFF";
+        ctx.strokestyle="#000";
+        if(state.current == state.game){
+            ctx.lineWidth = 2;
+            ctx.font = "35px Teko";
+            ctx.fillText(this.value, cvs.width/2,50);
+            ctx.strokeText(this.value, cvs.width/2,50);
+        }else if (state.current == state.over){
+            ctx.font = "25px Teko";
+            ctx.fillText(this.value, 225, 186);
+            ctx.strokeText(this.value, 225, 186);
+            ctx.fillText(this.best, 225,228);
+            ctx.strokeText(this.best, 225, 228);
+        }
+    },
+    reset : function(){
+        this.value = 0;
+    }
+}
 //DRAW
 
 function draw(){
@@ -205,14 +311,16 @@ function draw(){
     fg.draw();
     bird.draw();
     getReady.draw();
+    pipes.draw();
     gameOver.draw();
+    score.draw();
 }
 //UPDATE
 
 function update(){
 bird.update();
 fg.update();
-    
+pipes.update();   
 }
 //LOOP
 function loop(){
